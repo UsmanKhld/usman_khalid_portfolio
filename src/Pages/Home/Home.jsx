251e95ef7fed764fd503f707c74b4f0017542890
Usman Khalid <usmankhld602@gmail.com>
2024-12-24 11:10:45 -0600

    updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Navbar } from "../../Components/Navbar/Navbar";
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <main>
-        <div className="w-full h-screen border border-white"></div>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<main>
+				<div className="w-full h-screen border border-white flex items-center justify-center">
+					<div className="border border-white w-2/3 h-2/3 flex">
+						<h1 className="text-7xl">Hi I'm Usman</h1>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
